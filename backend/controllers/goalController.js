f3a59ee3c6266baa9194e16690fea6bd9b43be00
ekoferifah <191240000890@unisnu.ac.id>
2022-07-15 18:33:43 +0700

    Update a User Goal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -37,6 +37,19 @@ const updateGoal = asyncHandler (async(req,res) => {
         res.status(400)
         throw new Error('Goal not found')
     }
+
+    const user = await User.findById(req.user.id)
+
+    // check user
+    if(!user) {
+        res.status(401)
+        throw new Error ('User not found')
+    }
+    
+    if(goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error ('User not authorized')
+    }
     
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
 
@@ -52,6 +65,21 @@ const deleteGoal = asyncHandler (async(req,res) => {
         res.status(400)
         throw new Error ('Goal Not Found')
     }
+
+    const user = await User.findById(req.user.id)
+
+    // check user
+    if(!user) {
+        res.status(401)
+        throw new Error ('User not found')
+    }
+    
+    if(goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error ('User not authorized')
+    }
+    
+
     await goal.remove ()
 
     res.status(200).json({id: req.params.id})
